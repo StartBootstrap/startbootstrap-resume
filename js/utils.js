c5e83edb17a7225b13b504ea334abea0f14fd5ff
Peter Gorgone <peter@peltronic.com>
2018-03-01 17:54:18 -0800

    Add a scroll button

---
+++ js/utils.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+
+    var isBottomOnLoad = (window.innerHeight + window.scrollY) >= document.body.offsetHeight;
+
+    if (isBottomOnLoad) {
+        $('#tag-clickme_to_scroll_down h4.tag-top').show();
+    } else {
+        $('#tag-clickme_to_scroll_down h4.tag-more').show();
+    }
+
+    $(document).on('click', '#tag-clickme_to_scroll_down .tag-more a', function(e) {
+        var doc = document.documentElement;
+        var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+        var y = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
+        //window.scrollTo(0,document.body.scrollHeight);
+
+        //console.log(h,y);
+        $("html, body").animate({ scrollTop: h+y }, 750, function() {
+            var isBottom = (window.innerHeight + window.scrollY) >= document.body.offsetHeight;
+            if (isBottom) {
+                $('#tag-clickme_to_scroll_down .tag-more').hide();
+                $('#tag-clickme_to_scroll_down .tag-top').show();
+            } else {
+                $('#tag-clickme_to_scroll_down .tag-top').hide();
+                $('#tag-clickme_to_scroll_down .tag-more').show();
+            }
+        });
+
+        /*
+        setTimeout(function () {
+            window.scrollTo(0, h+y);
+        },2);
+        */
+    });
+
+    $(document).on('click', '#tag-clickme_to_scroll_down .tag-top a', function(e) {
+        $('#tag-clickme_to_scroll_down .tag-top').hide();
+        $("html, body").animate({ scrollTop: 0 }, 1000, function() {
+            $('#tag-clickme_to_scroll_down .tag-more').show();
+        });
+        return false;
+    });
+
+});
