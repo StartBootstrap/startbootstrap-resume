6b6e1b0fb96c9eebebfd42bcfce9cae32da1eadb
Peter Gorgone <peter@peltronic.com>
2019-01-21 12:30:38 -0800

    updated

--- js/utils.js
+++ js/utils.js
@@ -13,10 +13,12 @@ $(document).ready(function() {
         var doc = document.documentElement;
         var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
         var y = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
+        var scrollDelta = h+y;
+        //scrollDelta = 0.85*scrollDelta; // %NOTE: fudget factor
         //window.scrollTo(0,document.body.scrollHeight);
 
         //console.log(h,y);
-        $("html, body").animate({ scrollTop: h+y }, 750, function() {
+        $("html, body").animate({ scrollTop: scrollDelta }, 750, function() {
             var isBottom = (window.innerHeight + window.scrollY) >= document.body.offsetHeight;
             if (isBottom) {
                 $('#tag-clickme_to_scroll_down .tag-more').hide();
