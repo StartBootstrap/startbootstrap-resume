94eb742c1498f47e8e12ff2d9d581a8b22a2e4e3
Mayowa <mayowaadeniyi@hotmail.com>
2019-05-06 22:27:36 +0100

    Deleted portfolio, edited education, changed skills, edited interests;

---
+++ randomstuff.js
@@ -0,0 +1,78 @@
+<section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="portfolio">
+<div class="w-100">
+  <h2 class="mb-5">Portfolio</h2>
+  
+  <!-- <div class="mb-0">
+  <h3>Fashion Project
+    <img style="height: 50px" src='https://cdn.iconscout.com/icon/free/png-256/fashion-159-729038.png'>
+  </h3> 
+  <a href='https://github.com/itzcookiie/Fashion-Project'>View on github</a>
+  <ul class="fa-ul mb-0"><br>
+    <li>
+      A dynamic web app using React and Rails that lets users upload their wardrobe collection, create new outfits from them based on different styles and save those outfits online. User authentication is done using JWT tokens.</li>
+      <img class="img-fluid" src='img/fashion-app.jpg' >
+  </ul>
+</div> -->
+
+<div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+  <div class="resume-content">
+    <h3 class="mb-0">Fashion Nova
+    </h3>
+    <div class="mb-3">
+        <a href="http://www.fashionnova.com" target="_blank">Visit here</a>
+    </div>
+    <div>
+    <p>One of the most popular fashion design companies, Fashion Nova is a billion dollar retailer in the selling and campaigning of affordable fashion. Fashion Nova has stores all over the world with its headquarters located in America, California. I was part of the London team, involved in developing the website for the London franchise and adding a web portal for customers to upload their outfits online.
+      <br> 
+      Technology: React, Ruby on Rails, Postgre
+    </p>
+    <img style="height: 375px" src='img/fashionnova.PNG' >
+  </div>
+  </div>
+  <!-- <div class="resume-date text-md-right">
+    <span class="text-primary">March 2013 - Present</span>
+  </div> -->
+</div>
+
+<div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+    <div class="resume-content">
+      <h3 class="mb-0">Spring Care
+      </h3>
+      <div class="mb-3">
+        <!-- <p>View on github -->
+            <a href='http://springcarerecruitment.com/'>Visit here</a>
+        <!-- </p> -->
+      </div>
+      <div>
+      <p>A law firm for providing legal advice and helping clients with their claims and cases such as employment, immigration, debt, housing and crime. I helped launch their website from prototype to development, utilized techniques to promote user interaction and boost their USP as well as provide assistance in developing a clean codebase.
+        <br>
+        Technology: React, Javascript, Nodejs
+      </p>
+      <img src='img/springcare.PNG' style="height: 375px" >
+    </div>
+    </div>
+    <!-- <div class="resume-date text-md-right">
+      <span class="text-primary">March 2013 - Present</span>
+    </div> -->
+  </div>
+
+<div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+  <div class="resume-content">
+    <h3 class="mb-0">True Light Media
+    </h3>
+    <div class="mb-3">
+      <a href='http://www.truelightmedia.com/'>Visit Here</a>
+    </div>
+    <div>
+    <p>A charity for helping poor people in rural areas. I helped re-design the landing page to suit the client's needs and created a user portal for users to login and donate. <br>
+    Technology: React, Javascript, Nodejs</p>
+    <img style="height: 375px" src='img/TLM.PNG' >
+  </div>
+  </div>
+  <!-- <div class="resume-date text-md-right">
+    <span class="text-primary">March 2013 - Present</span>
+  </div> -->
+</div>
+
+</div>
+</section>
