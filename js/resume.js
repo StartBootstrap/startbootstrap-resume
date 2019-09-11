9d4198adf2abe2e29b66829a2bad1bda1987f2cd
Bailey Stein <bastein@umich.edu>
2019-09-10 23:32:15 -0400

    Begin adding project modal views

--- js/resume.js
+++ js/resume.js
@@ -25,4 +25,16 @@
     target: '#sideNav'
   });
 
+  // Modal popup$(function () {
+  $('.portfolio-item').magnificPopup({
+    type: 'inline',
+    preloader: false,
+    focus: '#username',
+    modal: true
+  });
+  $(document).on('click', '.portfolio-modal-dismiss', function(e) {
+    e.preventDefault();
+    $.magnificPopup.close();
+  });
+
 })(jQuery); // End of use strict
