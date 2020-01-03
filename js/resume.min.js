c1fbb548e22cfcdd61ab105bed24c15df528fddd
Adri <at_soamazed@icloud.com>
2020-01-03 03:43:22 -0500

    updating Experince

--- js/resume.min.js
+++ js/resume.min.js
@@ -1,6 +1,6 @@
 /*!
  * Start Bootstrap - Resume v5.0.8 (https://startbootstrap.com/template-overviews/resume)
- * Copyright 2013-2019 Start Bootstrap
+ * Copyright 2013-2020 Start Bootstrap
  * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-resume/blob/master/LICENSE)
  */
 
