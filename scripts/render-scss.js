9b16fa45281b3afb2d38bdd40d60342d1a064c78
Boris Damevin <borisdamevin@gmail.com>
2020-06-07 23:15:40 +0200

    feat: use sass-rem mixins

--- scripts/render-scss.js
+++ scripts/render-scss.js
@@ -1,36 +1,36 @@
 'use strict';
-const autoprefixer = require('autoprefixer')
+const autoprefixer = require('autoprefixer');
 const fs = require('fs');
 const packageJSON = require('../package.json');
 const path = require('path');
-const postcss = require('postcss')
+const postcss = require('postcss');
 const sass = require('sass');
 const sh = require('shelljs');
+const tildeImporter = require('node-sass-tilde-importer');
 
 const stylesPath = '../src/scss/styles.scss';
 const destPath = path.resolve(path.dirname(__filename), '../dist/css/styles.css');
 
 module.exports = function renderSCSS() {
-    
     const results = sass.renderSync({
         data: entryPoint,
+        importer: tildeImporter,
         includePaths: [
             path.resolve(path.dirname(__filename), '../node_modules')
-        ],
-      });
+        ]
+    });
 
     const destPathDirname = path.dirname(destPath);
     if (!sh.test('-e', destPathDirname)) {
         sh.mkdir('-p', destPathDirname);
     }
 
-    postcss([ autoprefixer ]).process(results.css, {from: 'styles.css', to: 'styles.css'}).then(result => {
+    postcss([autoprefixer]).process(results.css, { from: 'styles.css', to: 'styles.css' }).then(result => {
         result.warnings().forEach(warn => {
-            console.warn(warn.toString())
-        })
+            console.warn(warn.toString());
+        });
         fs.writeFileSync(destPath, result.css.toString());
-    })
-
+    });
 };
 
 const entryPoint = `/*!
@@ -39,4 +39,4 @@ const entryPoint = `/*!
 * Licensed under ${packageJSON.license} (https://github.com/BlackrockDigital/${packageJSON.name}/blob/master/LICENSE)
 */
 @import "${stylesPath}"
-`
+`;
