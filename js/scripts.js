09597da57d0a5c51247a7ef04da502531df2cf84
David Miller <hello@davidmiller.io>
2020-06-18 17:54:51 -0400

    update dependencies and push version

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 /*!
-    * Start Bootstrap - Resume v6.0.0 (https://startbootstrap.com/template-overviews/resume)
+    * Start Bootstrap - Resume v6.0.1 (https://startbootstrap.com/template-overviews/resume)
     * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-resume/blob/master/LICENSE)
+    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
     */
     (function ($) {
     "use strict"; // Start of use strict
