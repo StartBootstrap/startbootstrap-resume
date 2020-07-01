74cded8eecb9271671cb38c27c4410f9656337f8
dylangrandmont <dylan.grandmont@gmail.com>
2020-06-30 18:09:13 -0600

    remove jQuery scroll, use native functionality

--- js/resume.js
+++ js/resume.js
@@ -1,19 +1,6 @@
+
 (function($) {
   "use strict"; // Start of use strict
-
-  // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top)
-        }, 1000, "easeInOutExpo");
-        return false;
-      }
-    }
-  });
 
   // Closes responsive menu when a scroll trigger link is clicked
   $('.js-scroll-trigger').click(function() {
