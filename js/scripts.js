4e7383fb43e01348b61cc3679da6011110ddae8d
David Miller <hello@davidmiller.io>
2021-08-05 16:03:35 -0400

    Bootstrap 5.1.0 Update

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 /*!
-* Start Bootstrap - Resume v7.0.2 (https://startbootstrap.com/theme/resume)
+* Start Bootstrap - Resume v7.0.3 (https://startbootstrap.com/theme/resume)
 * Copyright 2013-2021 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
 */
